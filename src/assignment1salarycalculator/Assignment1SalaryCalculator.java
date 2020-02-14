5e37d4650372f4d72d26781ffed024a4f3d1b469
m vikram <kotagiri002@gannon.edu>
2020-02-14 00:54:40 -0500

    Completed Assignment1SalaryCalculator Structure

--- src/assignment1salarycalculator/Assignment1SalaryCalculator.java
+++ src/assignment1salarycalculator/Assignment1SalaryCalculator.java
@@ -5,17 +5,57 @@
  */
 package assignment1salarycalculator;
 
+import java.util.Scanner;
 /**
  *
  * @author gubotdev
  */
-public class Assignment1SalaryCalculator {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+public class Assignment1SalaryCalculator
+{
+  int salary;
+  int hourlyWage;
+  double taxes;
+
+
+  public int salary (int hourlyWage)
+  {
+    salary = hourlyWage * 2080;
+    int monthlySalary = salary / 12;
+      System.out.println ("Salary: ");
+      return salary;
+  }
+
+  public double taxes ()
+  {
+    taxes = (salary * 15) / 100;
+    System.out.println ("taxes: ");
+    return taxes;
+  }
+
+  public double monthlyAfterTax ()
+  {
+    double monthlyAfterTax = (salary - taxes) / 12;
+    System.out.println ("monthlyAfterTax: ");
+    return monthlyAfterTax;
+  }
+
+  public static void main (String[]args)
+  {
+    Assignment1SalaryCalculator cal = new Assignment1SalaryCalculator ();
+
+    Scanner myObj = new Scanner (System.in);
+
+    System.out.println ("Enter Hourly Wage");
+
+    int hourlyWage = myObj.nextInt ();
+
+    System.out.println ("Hourly Wage is: " + hourlyWage);
+
+    System.out.println (cal.salary (+hourlyWage));
+
+    System.out.println (cal.taxes ());
+
+    System.out.println (cal.monthlyAfterTax ());
+  }
 }
